f4afcd3818752ee32b2c376bb12d5238f89cec95
peopleinfo <49970466+peopleinfo@users.noreply.github.com>
2025-08-08 16:17:12 +0700

    add v-console

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,6 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+// Initialize vconsole for mobile debugging in development mode
+// if (import.meta.env.DEV) {
+  import('vconsole').then(({ default: VConsole }) => {
+    new VConsole()
+  })
+// }
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
