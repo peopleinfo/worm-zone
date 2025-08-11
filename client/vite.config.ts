435525ae4486ff4a04ceadb9a33d3da525b98a85
peopleinfo <49970466+peopleinfo@users.noreply.github.com>
2025-08-11 13:36:00 +0700

    disable single file build

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,13 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { viteSingleFile } from "vite-plugin-singlefile";
 
 export default defineConfig(() => {
   return {
-    plugins: [react(), viteSingleFile({ removeViteModuleLoader: true })],
-    base: "", // mini app compatibility
+    plugins: [react()],
     build: {
       minify: true,
     },
+    base: "./",
   };
 });
